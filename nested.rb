d6864c510c6a8d438caac1f3c339c916815a19db
Oleg Chursin <olegchursin@gmail.com>
2017-11-30 07:29:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 
+
 		 programmer_hash = {
 		 	yukihiro_matsumoto:
 		 		{ known_for: "Ruby",
@@ -109,6 +109,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+
+		 programmer_hash["alan_kay"]["known_for"] = alans_new_info
 
 
 end
